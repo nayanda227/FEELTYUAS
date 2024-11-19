f419d03055af1713ee128b1e57a96fd11892c42c
Nadia Alya Paramitha <nalya2911@gmail.com>
2024-11-19 19:19:14 +0700

    edit sigup form

--- lib/SignUp/components/signup_form.dart
+++ lib/SignUp/components/signup_form.dart
@@ -6,8 +6,8 @@ import '../../Login/login_screen.dart';
 
 class SignUpForm extends StatelessWidget {
   const SignUpForm({
-    Key? key,
-  }) : super(key: key);
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
